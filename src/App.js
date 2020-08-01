3eb2eae0549ea1a6ddc9c634047672299f4d9149
Anishaagupta <anisha8057@gmail.com>
2020-08-01 15:42:42 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
+import TodoList from './Components/container/TodoList/TodoList';
 
 const App = () => {
-  const [inputList, setInputList] = useState('Buy Apple');
+  const [inputList, setInputList] = useState('');
+  const [items, setItems] = useState([]);
 
-  const inputChange = () => {};
+  const inputChange = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList('');
+  };
+
+  const deleteItem = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElement, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div className='App'>
       <div className='main-div'>
@@ -13,10 +33,24 @@ const App = () => {
           <br />
           <h1>TASK MANAGER</h1>
           <br />
-          <input onChange={inputChange} type='text' placeholder='Add Task..' />
-          <button> + </button>
+          <input
+            value={inputList}
+            onChange={inputChange}
+            type='text'
+            placeholder='Add Task..'
+          />
+          <button onClick={listOfItems}> + </button>
           <ol>
-            <li>{inputList}</li>
+            {items.map((val, index) => {
+              return (
+                <TodoList
+                  key={index}
+                  id={index}
+                  onSelect={deleteItem}
+                  text={val}
+                />
+              );
+            })}
           </ol>
         </div>
       </div>
